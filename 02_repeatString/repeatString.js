a92efcb336c3448558ba4ac2f68eccfe740b6fba
Nathalia <nathalia96234@gmail.com>
2024-06-21 22:15:28 +0200

    repeatString adjusted

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+function repeatString(str, num) {
+    if (num < 0) {
+      return 'ERROR';
+    } else if (num === 0) {
+      return '';
+    } else {
+      let repeatedString = '';
+      for (let i = 0; i < num; i++) {
+        repeatedString += str;
+      }
+      return repeatedString;
+    }
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
