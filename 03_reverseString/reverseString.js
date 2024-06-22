c3efe68ad5e9d3b29d51afe8ac30f5c5608ca9fe
Nathalia <nathalia96234@gmail.com>
2024-06-22 19:14:18 +0200

    reverseString updated

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseString = splitString.reverse();
+    let joinArray = reverseString.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
