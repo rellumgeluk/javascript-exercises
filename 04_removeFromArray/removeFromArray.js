aa3e5f243002783de2cde399658ba1b4b0bc7b5c
Nathalia <nathalia96234@gmail.com>
2024-06-22 20:20:56 +0200

    04 updates

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args [0];
+    const newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)) {
+            newArray.push(item);
+            console.log(newArray);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
